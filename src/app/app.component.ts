fc34884e432a1be90e74541bd26656de90afbf59
suresh <suresh.avlm@gmail.com>
2019-06-23 10:03:17 +0530

    view children

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { AlertComponent } from './alert.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	template: `
+    <app-alert ok="Next" (close)="showAlert(2)">
+      Step 1: Demographics<br/>
+      First Name: <input type="text"><br/>
+      Last Name: <input type="text"><br/>
+    </app-alert>
+    <app-alert ok="Next" (close)="showAlert(3)">
+      Step 2: Criminal Information
+      <label>Have you been filed with any case in last 5 year?</label><br/>
+
+    </app-alert>
+    <app-alert ok="Close">
+      Step 3: Build app
+    </app-alert>
+	  <button (click)="showAlert(1)">Show steps</button>`
 })
+
 export class AppComponent {
-  title = 'moduletwo-viewchild';
+  @ViewChildren(AlertComponent) alerts: QueryList<AlertComponent>;
+  alertsArr = [];
+
+  ngAfterViewInit() {
+    this.alertsArr = this.alerts.toArray();
+  }
+  
+  showAlert(step) {
+    this.alertsArr[step - 1].show(); // step 1 is alert index 0
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AlertComponent
   ],
   imports: [
     BrowserModule
